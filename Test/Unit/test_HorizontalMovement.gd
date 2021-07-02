84b2bdff8d2e4a4f5422a5b652d649d8a587ef9a
DannyDarwiche <darwichedanny@gmail.com>
2021-07-02 16:32:55 +0200

    Refactor PlayerController to not call setter and getter functions
    WIP #5
    Refactor test_constructor_setting_move_speed_properly to be parameterized.
    Removed explicit calls to set and get functions in all tests.

--- Test/Unit/test_HorizontalMovement.gd
+++ Test/Unit/test_HorizontalMovement.gd
@@ -2,14 +2,18 @@ extends 'res://addons/gut/test.gd'
 
 var PlayerController = load("res://PlayerController.gd")
 
-func test_move_speed_is_always_positive_value():
-	var controller = PlayerController.new(10.0)
-	assert_between(controller.moveSpeedGet(), 0.0, INF)
-	
+var move_speed_constructor_values = [[10.0], [-10.0]]
+func test_constructor_setting_move_speed_properly(params = use_parameters(move_speed_constructor_values)):
+	var controller = PlayerController.new(params[0])
+
+	assert_between(controller.moveSpeed, 0.0, INF)
+
 	controller.free()
-	controller = PlayerController.new(-10.0)	
-	assert_between(controller.moveSpeedGet(), 0.0, INF)
+
+func test_setter_func_setting_move_speed_properly():
+	var controller = PlayerController.new(10.0)
+	controller.moveSpeed = -10.0
 	
-	controller.moveSpeedSet(-10.0)
-	assert_between(controller.moveSpeedGet(), 0.0, INF)
+	assert_between(controller.moveSpeed, 0.0, INF)
+	
 	controller.free()

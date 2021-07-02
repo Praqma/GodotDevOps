f8dd9fb3e1a778bf458dedc914bbfe73b7bb09cd
DannyDarwiche <darwichedanny@gmail.com>
2021-07-02 17:51:47 +0200

    Remove _init from PlayerController and refactor test to not use it
    WIP #5

--- Test/Unit/test_HorizontalMovement.gd
+++ Test/Unit/test_HorizontalMovement.gd
@@ -2,17 +2,10 @@ extends 'res://addons/gut/test.gd'
 
 var PlayerController = load("res://PlayerController.gd")
 
-var move_speed_constructor_values = [[10.0], [-10.0]]
-func test_constructor_setting_move_speed_properly(params = use_parameters(move_speed_constructor_values)):
-	var controller = PlayerController.new(params[0])
-
-	assert_between(controller.moveSpeed, 0.0, INF)
-
-	controller.free()
-
-func test_setter_func_setting_move_speed_properly():
-	var controller = PlayerController.new(10.0)
-	controller.moveSpeed = -10.0
+var move_speed_setter_values = [[10.0], [-10.0]]
+func test_setter_func_setting_move_speed_properly(params = use_parameters(move_speed_setter_values)):
+	var controller = PlayerController.new()
+	controller.moveSpeed = params[0]
 	
 	assert_between(controller.moveSpeed, 0.0, INF)
 	

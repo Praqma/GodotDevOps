b21f61d65a5b9f834743e7ce41795068213d71b9
DannyDarwiche <darwichedanny@gmail.com>
2021-07-02 17:53:52 +0200

    Add horizontal movement to player
    Fixes #5
    Add input actions for left, right, up, down and jump.
    Add test scene for trying movement.

--- PlayerController.gd
+++ PlayerController.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 export var moveSpeed := 300.0 setget moveSpeedSet, moveSpeedGet
+var moveInput := Vector2.ZERO
 
 func moveSpeedSet(value : float):
 	moveSpeed =  max(value, 0)
@@ -8,3 +9,9 @@ func moveSpeedSet(value : float):
 func moveSpeedGet() -> float:
 	return moveSpeed
 
+func _process(delta):
+		moveInput.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+
+func _physics_process(delta):
+	var velocity := Vector2(moveInput * moveSpeed)
+	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP, true)

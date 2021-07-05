98d6c4036ae2f54c200dcac1cbcdbf7917871a79
IsakN <isaq.tokyo@gmail.com>
2021-07-05 12:15:34 +0200

    Add jump
    WIP #6

--- PlayerController.gd
+++ PlayerController.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
 export var moveSpeed := 300.0 setget moveSpeedSet, moveSpeedGet
-export var gravity := 100.0 setget gravitySet, gravityGet
+export var gravity := 1000.0 setget gravitySet, gravityGet
+export var jumpForce := 500.0 setget jumpForceSet, jumpForceGet
 
 var moveInput := Vector2.ZERO
 var velocity := Vector2.ZERO
@@ -18,12 +19,22 @@ func gravitySet(value : float):
 func gravityGet() -> float:
 	return gravity
 
+func jumpForceSet(value : float):
+	jumpForce = value
+
+func jumpForceGet() -> float:
+	return jumpForce
+	
 func _process(delta):
 		moveInput.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 
 func _physics_process(delta):
 	velocity.x = moveInput.x * moveSpeed
-	
+	if Input.is_action_just_pressed("jump") :
+		jump()
 	velocity.y += gravity * delta
 	
 	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP, true)
+
+func jump():
+	velocity.y = -jumpForce

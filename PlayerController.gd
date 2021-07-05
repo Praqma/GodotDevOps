3552e731cc053519f3b6bb49458249f46d4af8cf
IsakN <isaq.tokyo@gmail.com>
2021-07-05 10:49:06 +0200

    Add gravity and test_VerticalMovement
    WIP #6

--- PlayerController.gd
+++ PlayerController.gd
@@ -1,7 +1,10 @@
 extends KinematicBody2D
 
 export var moveSpeed := 300.0 setget moveSpeedSet, moveSpeedGet
+export var gravity := 100.0 setget gravitySet, gravityGet
+
 var moveInput := Vector2.ZERO
+var velocity := Vector2.ZERO
 
 func moveSpeedSet(value : float):
 	moveSpeed =  max(value, 0)
@@ -9,9 +12,18 @@ func moveSpeedSet(value : float):
 func moveSpeedGet() -> float:
 	return moveSpeed
 
+func gravitySet(value : float):
+	gravity = value
+
+func gravityGet() -> float:
+	return gravity
+
 func _process(delta):
 		moveInput.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 
 func _physics_process(delta):
-	var velocity := Vector2(moveInput * moveSpeed)
+	velocity.x = moveInput.x * moveSpeed
+	
+	velocity.y += gravity * delta
+	
 	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP, true)

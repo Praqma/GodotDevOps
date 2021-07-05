28730f08c76359f3e766fbfdf83a5d42f1af0fa9
DannyDarwiche <darwichedanny@gmail.com>
2021-07-05 16:00:55 +0200

    Refactor movement speed variables, snap length value and jump if statement
    WIP #8
    Removed export of moveSpeed and made airMoveSpeed and groundMoveSpeed exported instead.
    Made the snap distance configurable through an exported variable.
    Added an additional bool statement to the if statement for jumping preventing jumps when walking off edges.

--- PlayerController.gd
+++ PlayerController.gd
@@ -1,15 +1,15 @@
 extends KinematicBody2D
 
-export var moveSpeed := 300.0 setget set_moveSpeed, get_moveSpeed
+export var airMoveSpeed := 100.0
+export var groundMoveSpeed := 300.0
 export var gravity := 1000.0 setget set_gravity, get_gravity
 export var jumpForce := 600.0 setget set_jumpForce, get_jumpForce
-
-var airMoveSpeed := 100.0
-var groundMoveSpeed := 300.0
+export var snapLength := 5000.0
 
 var snap := false
 var moveInput := Vector2.ZERO
 var velocity := Vector2.ZERO
+var moveSpeed := 300.0 setget set_moveSpeed, get_moveSpeed
 
 func set_moveSpeed(value : float):
 	moveSpeed =  max(value, 0)
@@ -34,11 +34,13 @@ func _process(delta):
 
 func _physics_process(delta):
 	velocity.x = moveInput.x * moveSpeed
-	if Input.is_action_just_pressed("jump") and snap:
+	
+	if Input.is_action_just_pressed("jump") and snap and is_on_floor():
 		jump()
+	
 	velocity.y += gravity * delta
 	
-	var snapVector = Vector2.DOWN * 50 if snap else Vector2()
+	var snapVector = Vector2.DOWN * snapLength if snap else Vector2()
 	velocity = move_and_slide_with_snap(velocity, snapVector, Vector2.UP, true)
 	
 	var just_landed := is_on_floor() and not snap

640246d80476be566ae951c0e3da91648c3c0ef2
IsakN <isaq.tokyo@gmail.com>
2021-07-05 12:15:34 +0200

    Add jump
    WIP #6

--- Test/Unit/test_VerticalMovement.gd
+++ Test/Unit/test_VerticalMovement.gd
@@ -14,3 +14,17 @@ func test_gravity_updates_properly(params = use_parameters(gravity_update_values
 	controller.free()
 # Gravity
 # Jump
+
+var jump_update_values = [[10, .1, -100.0], [5,1,300.0], [1, 2, 0.0]]
+func test_jump_velocity_updates_properly(params = use_parameters(jump_update_values)) :
+	var controller = PlayerController.new()
+	controller.gravity = 100.0
+	controller.jumpForce = 200.0
+	
+	controller.jump();
+	
+	simulate(controller, params[0], params[1])
+	
+	assert_eq(controller.velocity.y, params[2])
+	
+	controller.free()

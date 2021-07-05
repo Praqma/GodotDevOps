c05183eae666740279db06a272bac08952bb698f
IsakN <isaq.tokyo@gmail.com>
2021-07-05 10:49:06 +0200

    Add gravity and test_VerticalMovement
    WIP #6

---
+++ Test/Unit/test_VerticalMovement.gd
@@ -0,0 +1,16 @@
+extends 'res://addons/gut/test.gd'
+
+var PlayerController = load("res://PlayerController.gd")
+
+var gravity_update_values = [[1, 1, 100.0], [5,1,500.0], [1, 5, 500.0]]
+func test_gravity_updates_properly(params = use_parameters(gravity_update_values)) :
+	var controller = PlayerController.new()
+	controller.gravity = 100.0
+	
+	simulate(controller, params[0], params[1])
+	
+	assert_eq(controller.velocity.y, params[2])
+	
+	controller.free()
+# Gravity
+# Jump

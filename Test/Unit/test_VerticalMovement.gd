34da9cd163eaa85f36f10684084450f0b71353b3
DannyDarwiche <darwichedanny@gmail.com>
2021-07-06 15:14:37 +0200

    Refactor vertical movement tests to use before_each and after_each
    WIP #7

--- Test/Unit/test_VerticalMovement.gd
+++ Test/Unit/test_VerticalMovement.gd
@@ -1,10 +1,16 @@
 extends 'res://addons/gut/test.gd'
 
 var PlayerController = load("res://PlayerController.gd")
+var controller
+
+func before_each():
+	controller = PlayerController.new()
+
+func after_each():
+	controller.free()
 
 var expected_jump_values = [[1.0, -100.0], [5.0, 300.0], [2.0, 0.0]]
 func test_jump_velocity_updates_properly(params = use_parameters(expected_jump_values)) :
-	var controller = PlayerController.new()
 	controller.gravity = 100.0
 	controller.jumpForce = 200.0
 	
@@ -12,16 +18,11 @@ func test_jump_velocity_updates_properly(params = use_parameters(expected_jump_v
 	controller.calculate_velocity(params[0])
 	
 	assert_eq(controller.velocity.y, params[1])
-	
-	controller.free()
 
 var expected_gravity_values = [[1.0, 100.0], [5.0, 500.0]]
 func test_calculate_vertical_velocity_with_gravity(params = use_parameters(expected_gravity_values)):
-	var controller = PlayerController.new()
 	controller.gravity = 100.0
 	
 	controller.calculate_velocity(params[0])
 	
 	assert_eq(controller.velocity.y, params[1])
-	
-	controller.free()

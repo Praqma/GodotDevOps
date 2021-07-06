9fb9f56e9a1dbd802f42d914405e243509dbaace
DannyDarwiche <darwichedanny@gmail.com>
2021-07-06 14:57:40 +0200

    Refactor player controller and vertical movement tests
    Fixes #17
    
    Velocity is calculated in a separate function outside of _physics_process and jumping is handled in _process now instead.
    Tests have been rewritten to not use simulate and is no longer generating errors.

--- Test/Unit/test_VerticalMovement.gd
+++ Test/Unit/test_VerticalMovement.gd
@@ -2,27 +2,26 @@ extends 'res://addons/gut/test.gd'
 
 var PlayerController = load("res://PlayerController.gd")
 
-var gravity_update_values = [[1, 1, 100.0], [5,1,500.0], [1, 5, 500.0]]
-func test_gravity_updates_properly(params = use_parameters(gravity_update_values)) :
-	var controller = PlayerController.new()
-	controller.gravity = 100.0
-	
-	simulate(controller, params[0], params[1])
-	
-	assert_eq(controller.velocity.y, params[2])
-	
-	controller.free()
-
-var jump_update_values = [[10, .1, -100.0], [5,1,300.0], [1, 2, 0.0]]
-func test_jump_velocity_updates_properly(params = use_parameters(jump_update_values)) :
+var expected_jump_values = [[1.0, -100.0], [5.0, 300.0], [2.0, 0.0]]
+func test_jump_velocity_updates_properly(params = use_parameters(expected_jump_values)) :
 	var controller = PlayerController.new()
 	controller.gravity = 100.0
 	controller.jumpForce = 200.0
 	
 	controller.jump();
+	controller.calculate_velocity(params[0])
 	
-	simulate(controller, params[0], params[1])
+	assert_eq(controller.velocity.y, params[1])
 	
-	assert_eq(controller.velocity.y, params[2])
+	controller.free()
+
+var expected_gravity_values = [[1.0, 100.0], [5.0, 500.0]]
+func test_calculate_vertical_velocity_with_gravity(params = use_parameters(expected_gravity_values)):
+	var controller = PlayerController.new()
+	controller.gravity = 100.0
+	
+	controller.calculate_velocity(params[0])
+	
+	assert_eq(controller.velocity.y, params[1])
 	
 	controller.free()

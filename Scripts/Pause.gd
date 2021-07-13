d277f48a8aeb18b2e391dacf00b9e93830a6334d
IsakN <isaq.tokyo@gmail.com>
2021-07-13 11:50:08 +0200

    Make so the resume button resumes the game
    WIP #52

--- Scripts/Pause.gd
+++ Scripts/Pause.gd
@@ -5,3 +5,9 @@ func _input(event):
 		var new_pause_state = not get_tree().paused
 		get_tree().paused = new_pause_state
 		visible = new_pause_state
+
+
+func _on_Resume_pressed():
+	var new_pause_state = not get_tree().paused
+	get_tree().paused = new_pause_state
+	visible = new_pause_state

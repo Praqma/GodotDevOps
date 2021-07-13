36f86f6575725925307b6e207bfc021a10c749e5
IsakN <isaq.tokyo@gmail.com>
2021-07-13 11:53:52 +0200

    Make Menu button return to mainmenu
    WIP #52

--- Scripts/Pause.gd
+++ Scripts/Pause.gd
@@ -1,5 +1,7 @@
  extends Control
 
+onready var _transition_rect := $"../SceneTransitionRect"
+
 func _input(event):
 	if event.is_action_pressed("pause") :
 		var new_pause_state = not get_tree().paused
@@ -8,6 +10,13 @@ func _input(event):
 
 
 func _on_Resume_pressed():
+	var new_pause_state = not get_tree().paused
+	get_tree().paused = new_pause_state
+	visible = new_pause_state
+
+
+func _on_Menu_pressed():
+	_transition_rect.transition_to("res://Scenes/MainMenu.tscn")
 	var new_pause_state = not get_tree().paused
 	get_tree().paused = new_pause_state
 	visible = new_pause_state

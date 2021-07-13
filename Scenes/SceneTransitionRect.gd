0a7af25633630a0be7df758a7e40151b6c8db3bc
DannyDarwiche <darwichedanny@gmail.com>
2021-07-13 13:22:37 +0200

    Stops pausing during scene transition
    Fixes #52

--- Scenes/SceneTransitionRect.gd
+++ Scenes/SceneTransitionRect.gd
@@ -1,11 +1,16 @@
 extends ColorRect
 
+var isTransitioning := false
+
 onready var _anim_player := $AnimationPlayer
 
 func _ready():
 	_anim_player.play_backwards("Fade");
 
 func transition_to(_next_scene) -> void:
+	isTransitioning = true
 	_anim_player.play("Fade")
 	yield(_anim_player, "animation_finished")
 	get_tree().change_scene(_next_scene)
+	get_tree().paused = false
+	isTransitioning = false

9469aa6f86a15ed22d946180cb7013b4038e06eb
IsakN <isaq.tokyo@gmail.com>
2021-07-13 11:57:53 +0200

    Refactor pause script
    WIP #52
    
    all pause logic was separated into its own function so that the other functions just have to call that instead of doing all the logic themselves

--- Scripts/Pause.gd
+++ Scripts/Pause.gd
@@ -4,19 +4,16 @@ onready var _transition_rect := $"../SceneTransitionRect"
 
 func _input(event):
 	if event.is_action_pressed("pause") :
-		var new_pause_state = not get_tree().paused
-		get_tree().paused = new_pause_state
-		visible = new_pause_state
-
+		pause()
 
 func _on_Resume_pressed():
-	var new_pause_state = not get_tree().paused
-	get_tree().paused = new_pause_state
-	visible = new_pause_state
-
+	pause()
 
 func _on_Menu_pressed():
 	_transition_rect.transition_to("res://Scenes/MainMenu.tscn")
+	pause()
+
+func pause():
 	var new_pause_state = not get_tree().paused
 	get_tree().paused = new_pause_state
 	visible = new_pause_state

c8645d8ca08272b2d346a62a0249a3e4c5a4d4c7
IsakN <isaq.tokyo@gmail.com>
2021-07-26 16:33:16 +0200

    Add camera script with rotation logic
    WIP #86

---
+++ Scripts/Enemy/Camera.gd
@@ -0,0 +1,17 @@
+extends Sprite
+
+export var rotates := false
+export(float, 0.0, 1.0) var rotateSpeed := 1.0
+export(Array, int, -90, 90) var rotateArc := [0,45]
+
+onready var cameraHead = $CameraHead
+
+func _process(delta):
+	if rotates:
+		cameraHead.rotate(rotateSpeed * delta)
+		if cameraHead.rotation >= deg2rad(rotateArc.back()) :
+			rotateSpeed *= -1
+		elif cameraHead.rotation <= deg2rad(rotateArc[0]) :
+			rotateSpeed *= -1
+		print(cameraHead.rotation)
+		print(rotateSpeed)

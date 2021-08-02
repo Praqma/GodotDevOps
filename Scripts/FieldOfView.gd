9f384af5c5e2d916d87092bbced1457efad81ef9
DannyDarwiche <darwichedanny@gmail.com>
2021-08-02 13:32:58 +0200

    Add raycasts over cone area with debug lines
    WIP #103

--- Scripts/FieldOfView.gd
+++ Scripts/FieldOfView.gd
@@ -7,9 +7,13 @@ export(int, LAYERS_3D_PHYSICS) var collisionLayer = 1
 export var fovColor := Color(1,1,1,1)
 export var losUndetectColor := Color(1,1,0,1)
 export var losDetectColor := Color(1,0,0,1)
+# Number of rays per degree
+export (float) var coneResolution := 1.0
 
 var targets = PoolVector2Array()
 var losRange : float = 0
+
+var debugAngles := []
 
 func find_targets_inside_radius(entities) -> PoolVector2Array:
 	targets = PoolVector2Array()
@@ -36,6 +40,7 @@ func find_targets_with_raycast(entities) -> PoolVector2Array:
 	return targets
 
 func _process(delta):
+	draw_field_of_view()
 	update()
 
 func _physics_process(delta):
@@ -57,10 +62,23 @@ func _physics_process(delta):
 		losRange -= losSpeed
 	losRange = clamp(losRange, 0, detectRadius)
 
+func draw_field_of_view():
+	var stepCount = round(viewAngle * coneResolution)
+	var stepAngleSize = viewAngle / stepCount
+	
+	debugAngles.clear()
+	for i in stepCount:
+		var angle = -viewAngle/2 + stepAngleSize * i
+		debugAngles.append(angle)
+
 func _draw():
 	for target in targets:
 		draw_line(Vector2.ZERO, to_local(target), losUndetectColor)
 		draw_line(Vector2.ZERO, to_local(target).normalized() * losRange, losDetectColor, 2)
+	
+	for a in debugAngles:
+		var dir := Vector2(cos(deg2rad(a)), sin(deg2rad(a)))
+		draw_line(Vector2.ZERO, dir.normalized() * detectRadius, Color.blue)
 	
 	draw_line(Vector2.ZERO, Vector2(cos(deg2rad(-viewAngle/2)) * detectRadius, sin(deg2rad(-viewAngle/2)) * detectRadius), fovColor)
 	draw_line(Vector2.ZERO, Vector2(cos(deg2rad(viewAngle/2)) * detectRadius, sin(deg2rad(viewAngle/2)) * detectRadius), fovColor)

a5c8b3891ee29d9e00a83240dde1f24c0c51902b
DannyDarwiche <darwichedanny@gmail.com>
2021-08-06 15:19:22 +0200

    Add deactivated cameras with hidden fov
    Fixes #128 > New Feature: Deactivated cameras no longer show field of view making it easier to tell which cameras are active

--- Scripts/Interactables/ReceiverCamera.gd
+++ Scripts/Interactables/ReceiverCamera.gd
@@ -2,15 +2,20 @@ extends Node2D
 
 export(int) var id := 0
 export (Array, NodePath) var nodePaths
+export(NodePath) var fovPolygonPath : NodePath
 
 var active := false
 
 func change_state() -> void:
 	active = not active
-	for nodePath in nodePaths:
-		if active:
+	
+	if active:
+		get_node(fovPolygonPath).visible = false
+		for nodePath in nodePaths:
 			get_node(nodePath).set_process(false)
 			get_node(nodePath).set_physics_process(false)
-		else:
+	else:
+		get_node(fovPolygonPath).visible = true
+		for nodePath in nodePaths:
 			get_node(nodePath).set_process(true)
 			get_node(nodePath).set_physics_process(true)

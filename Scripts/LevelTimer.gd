37f4937bed183206b9735b038645fb4922f7158b
DannyDarwiche <darwichedanny@gmail.com>
2021-08-19 18:07:32 +0200

    Add timer scene
    WIP #144

---
+++ Scripts/LevelTimer.gd
@@ -0,0 +1,19 @@
+extends Control
+
+var time : float = 0.0
+var runTimer : bool = false
+
+onready var label = get_node("Label")
+
+func _process(delta):
+	if runTimer:
+		time += delta
+		
+		var mils = fmod(time, 1) * 1000
+		var secs = fmod(time, 60)
+		var mins = floor(time / 60)
+		
+		label.text = "%02d:%02d:%03d" % [mins, secs, mils]
+
+func _ready():
+	runTimer = true
